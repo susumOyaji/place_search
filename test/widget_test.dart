3e91d605acadc0206e963db688b0ec609850e7c1
susumOyaji <sumitomo0210@gmail.com>
2022-11-07 10:14:49 +0900

    up

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:place_search/main.dart';
+import 'package:place_search/main1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
